2ae4353c481f87a3b4d4bef5f624a73803df19a1
Mariah <mariahschock@gmail.com>
2022-06-13 14:48:44 -0700

    slogan input

--- src/App.js
+++ src/App.js
@@ -9,10 +9,11 @@ import CityNameInput from './CityNameInput';
 import City from './City';
 
 function App() {
-  const [castle, setCastle] = useState('Mysterious');
-  const [skyline, setSkyline] = useState('Foggy');
-  const [waterfront, setWaterfront] = useState('Sunset');
+  const [castle, setCastle] = useState('castle-1');
+  const [skyline, setSkyline] = useState('skyline-1');
+  const [waterfront, setWaterfront] = useState('waterfront-1');
   const [cityName, setCityName] = useState('');
+  const [sloganInput, setSloganInput] = useState('');
   // track some state here.
   // You'll need to keep track of a skylineId, waterfrontId, and castleId. All these start out as 1
   // you'll need to track a city name, which starts as the city name of your choice.
@@ -27,11 +28,12 @@ function App() {
       />  
       {/* here, the City component takes in skylineId, waterfrontId, castleId as props. It'll use those ids to render pictures correctly. */}
       <h1>
-        <CityNameInput setCityName={ setCityName } />
+        
         {/* dynamically update the city name here using state */}
         Welcome to beautiful {`${cityName}`}!
       </h1>
       <div className='bottom'>
+        <CityNameInput setCityName={ setCityName } />
         {/* here, the CityNameInput component takes in the setCityName state handler function */}
         <section className='dropdowns'>
           <WaterfrontDropdown setWaterfront={ setWaterfront } />
@@ -45,6 +47,7 @@ function App() {
           In each Dropdown, pass as props the state handler functions defined above, so that these child components can manipulate parent state 
           */}
         </section>
+        <SloganForm setSloganInput={ setSloganInput } />
         {/* here, the SloganForm component takes in the setSlogans state handler function and the slogans array that live in state */}
         {/* here, the SloganList component takes the array of slogans that lives in state */}
 
--- src/SloganForm.js
+++ src/SloganForm.js
@@ -1,9 +1,10 @@
-export default function SloganForm(props) {
+export default function SloganForm({ sloganInput, setSloganInput }) {
   // React forms are a pain! 
   // Track the sloganInput form state with a useState hook
     
   function handleSubmit(e) {
     e.preventDefault();
+
 
     // This component takes in a prop called setSlogans, which is a function that takes in an array of slogans. 
     // Also, this component also takes in a prop called slogans, which is an arry of strings. //// On submit call the setSlogans state with a new array that is a copy of the old array with the new slogan immutably added to the end 
@@ -14,11 +15,11 @@ export default function SloganForm(props) {
   return (
     <section>
       {/* on submit, call the handleSubmit function, defined above */}
-      <form>
+      <form onSubmit={handleSubmit}>
         Add a slogan to the list!
         {/* on change, set the sloganInput in state to be the input value */}
         {/* also, weirdly, set the value of this input to the sloganInput tracked in state to make this a 'controlled' form input */}
-        <input />
+        <input value={sloganInput} onChange={e => setSloganInput(e.target.value)} />
         <button>Submit</button>
       </form>
     </section>
